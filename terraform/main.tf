1af9dd7deeecb17d8312e27f7a32500d31f5765a
Peter Murray <681306+peter-murray@users.noreply.github.com>
2022-10-21 11:15:15 +0000

    Adding workflows

---
+++ terraform/main.tf
@@ -0,0 +1,43 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.28.0"
+    }
+  }
+  required_version = ">= 0.14.9"
+}
+
+provider "azurerm" {
+  features {}
+}
+
+# Generate a random integer to create a globally unique name
+resource "random_integer" "ri" {
+  min = 10000
+  max = 99999
+}
+
+# Create the resource group
+data "azurerm_resource_group" "rg" {
+  name     = var.azure_resource_group
+}
+
+data "azurerm_service_plan" "appserviceplan" {
+  name                = var.azure_service_plan
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_linux_web_app" "webapp" {
+  name                  = "webapp-${var.environment_name}-${random_integer.ri.result}"
+  location              = azurerm_resource_group.rg.location
+  resource_group_name   = azurerm_resource_group.rg.name
+  service_plan_id       = azurerm_service_plan.appserviceplan.id
+
+  site_config {
+    application_stack {
+      docker_image = var.container_image
+      docker_image_tag = var.container_tag
+    }
+  }
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,14 @@
+variable "azure_resource_group" {
+  type        = string
+  description = "Azure resource group name"
+}
+
+variable "azure_service_plan" {
+  type        = string
+  description = "Azure service plan"
+}
+
+variable "environment_name" {
+  type        = string
+  description = "Environment"
+}
